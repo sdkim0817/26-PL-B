95365e94f992f9c044733f0bc7a78ef0a7cf0d7b
jiu <>
2026-03-23 13:11:54 +0900

    first commit

---
+++ timetest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+	clock_t start, stop;
+	double duration;
+	start = clock();	// 측정 시작
+	for (int i = 0; i < 1000000; i++){
+        printf("%d", i);
+    }	
+	stop = clock();	// 측정 종료
+	duration = (double)(stop - start) / CLOCKS_PER_SEC;
+	printf("수행시간은 %f초입니다.\n", duration);
+	return 0;
+}
